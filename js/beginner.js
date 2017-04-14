fbaf04c7a76c889169f96801013767c5a62aa760
Preda Anuvatnujotikul <preda.anu@gmail>
2017-04-14 16:23:06 -0700

    Completed part 2.

--- js/beginner.js
+++ js/beginner.js
@@ -1,16 +1,47 @@
 var app = angular.module('myApp', []);
 
-app.run(function($rootScope) {
-  $rootScope.name = 'Ari Lerner';
-});
+// app.run(function($rootScope) {
+//   $rootScope.name = 'Ari Lerner';
+// });
+
+// app.controller('MyController', function($scope) {
+//   $scope.person = {
+//     name: 'Ari Lerner'
+//   };
+// });
+
+// app.controller('PlayerController', ['$scope', function($scope) {
+//   var audio = document.createElement('audio');
+//   $scope.audio = audio;
+
+//   audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+//   audio.play();
+// }]);
+
+// app.controller('RelatedController', ['$scope', function($scope) {
+// }]);
 
 app.controller('PlayerController', ['$scope', function($scope) {
-  var audio = document.createElement('audio');
-  $scope.audio = audio;
-
-  audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-  audio.play();
+  // var audio = document.createElement('audio');
+  // audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+  $scope.playing = false;
+  $scope.play = function() {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+  $scope.stop = function() {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop()
+    });
+  });
 }]);
 
 app.controller('RelatedController', ['$scope', function($scope) {
+
 }]);
